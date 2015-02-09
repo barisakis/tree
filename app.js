d9614814e6dc01d1959a19050c3587c14d6db876
Baris Akis <barisakis@DN0a227079.SUNet>
2015-02-08 23:11:24 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict";
+var express = require('express');
+var app = express();
+
+var auth = express.basicAuth('testUser', 'testPass');
+
+var nconf = require('nconf');
+nconf.argv().env().file({ file: 'local.json' });
+
+require('./settings')(app, express, nconf);
+require('./routes')(app, nconf);
+
+
+// var Sequelize = require('sequelize')
+//   , sequelize = new Sequelize('treedb', 'barisakis', 'password', {
+//       dialect: "postgres",
+//       port: 5432,
+//     })
+ 
+// sequelize
+//   .authenticate()
+//   .complete(function(err) {
+//     if (!!err) {
+//       console.log('Unable to connect to the database:', err)
+//     } else {
+//       console.log('Connection has been established successfully.')
+//     }
+//   })
+
+var port = process.env.PORT || nconf.get('port');
+console.log('Listening at 127.0.0.1:' + port);
+app.listen(port);
---
+++ routes/index.js
@@ -0,0 +1,109 @@
+/**
+ * Creates the routes for the given express application.
+ *
+ * @param app - the express application
+ * @param nconf - the configuration settings
+ */
+
+var https = require('https');
+var soundCloud = require('../lib/sound-cloud');
+var my_linkedin = require('../lib/linkedin');
+var express = require('express');
+
+// var Linkedin = require('node-linkedin')('api', 'secret', 'callback');
+// var linkedin = Linkedin.init('756wgw2gu8cu11');
+
+
+module.exports = function(app, nconf) {
+
+  var auth_associate = express.basicAuth('tree', 'associate');
+  //http://blog.modulus.io/nodejs-and-express-basic-authentication
+  var auth_candidate = express.basicAuth('tree', 'welcome');
+
+  app.get('/', function(request, response) {
+    response.render('main.html');
+  });
+
+
+  app.get('/team', function(request, response) {
+    response.render('team.html');
+  });
+
+  app.get('/client', function(request, response) {
+    response.render('client_form.html');
+  });
+
+  app.get('/candidate', function(request, response) {
+    response.render('no_li_candidate_form.html');
+  });
+
+  app.get('/post_meeting', auth_candidate, function(request, response) {
+    response.render('candidate_questions.html');
+  });
+
+  app.get('/contact', function(request, response) {
+    response.render('contact.html');
+  });
+
+  app.get('/interview', auth_associate, function(request, response) {
+    response.render('interview_form.html');
+  });
+
+  app.get('/join', function(request, response) {
+    response.render('associate_form.html');
+  });
+
+
+//LINKEDIN TESTS
+  app.get('/candidate_li', function(request, response) {
+    response.render('candidate_form.html');
+  });
+
+  app.get('/test', function(request, response) {
+    response.render('test.html');
+  });
+
+
+  app.get('/candidate_in', function(request, response) {
+    console.log("GET code & state");
+    // response.render('candidate_form.html');
+    var AUTHORIZATION_CODE = request.query.code;
+    console.log(AUTHORIZATION_CODE);
+    var STATE = request.query.state;
+    console.log(STATE);
+
+    if (!AUTHORIZATION_CODE || !STATE) {
+      response.send('Linkedin Login Problem', 422);
+    } else {
+      my_linkedin.import(AUTHORIZATION_CODE, STATE, function(error, body) {
+        if (error) {
+          throw error;
+        } else {
+          response.send(body, 200);
+        }
+      });
+    }
+  });
+
+  // app.get('/oauth/linkedin', function(req, res) {
+  //     // This will ask for permisssions etc and redirect to callback url.
+  //     Linkedin.auth.authorize(res, ['r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_contactinfo']);
+  // });
+
+  // app.get('/oauth/linkedin/callback', function(req, res) {
+  //     Linkedin.auth.getAccessToken(res, req.query.code, function(err, results) {
+  //         if ( err )
+  //             return console.error(err);
+
+  //         /**
+  //          * Results have something like:
+  //          * {"expires_in":5184000,"access_token":". . . ."}
+  //          */
+
+  //         console.log(results);
+  //         return res.redirect('/');
+  //     });
+  // });
+
+
+};
